40920eedd21568dfc81150e550bfad5bd22f740e
sarahvar <sarahvarin95@gmail.com>
2024-08-21 23:02:19 +0200

    Maxine

--- Max.php
+++ Max.php
@@ -11,13 +11,13 @@
     include('header.php');
     ?>
     <div class="maxbiche">
-        <img src="https://img1.psthc.fr/uploads/442145LifeisStrange17.jpg">
+        <img src="/assets/Max-Biche.webp">
     </div>
     <h1 class="bleu center">Maxine Caulfield</h1>
     <h2>Biographie de Max : </h2>
-    <img class="imgmax"><img src="https://www.nautiljon.com/images/jeuxvideo_persos/00/23/maxine_caulfield_2632.jpg">
+    <img class="imgmax"><img src="/assets/Maxine-Caulfield.webp">
     <div class="selfiemax">
-        <img src=https://www.tremplin-numerique.org/wp-content/uploads/2021/03/thumb-1920-642030.jpg height="300">
+        <img src="/assets/Max-selfies.webp" height="300">
     </div>
     <p>Max est une jeune femme née le  21 septembre 1995 à Arcadia Bay,l'Oregon aux Etats Unis. Elle est la fille de Vanessa et Ryan Caulfield. A 13 ans en 2008, elle démenage à Seattle avec ses parents suite à la mutation de son père. Elle revient seule 5 ans plus tard à Arcadia Bay pour étudier la photographie à l'académie de Blackwell. Pendant l'histoire du jeu elle a 18 ans. Elle est passionnée de photographie et l'univers de la science fiction.
         </p>
@@ -33,7 +33,7 @@
             <li>Yeux bleus</li>
         </ul>
         <div class="maxguitare">
-            <img src="https://th.bing.com/th/id/R.67f60b83ece470d93306e49d9f54d97c?rik=bY4cbAs0trqzpQ&pid=ImgRaw&r=0" height="200">
+            <img src="Max-Guitare" height="200">
         </div>
         <h3>Son rôle dans l'histoire: </h3>
 
@@ -54,51 +54,51 @@
     </thead>
     <tbody>
         <tr>
-            <td><b>Chloe, Elizabeth Price</b><br><img src=https://th.bing.com/th/id/R.9401d6e5e209aa020f6344424b5078d0?rik=9PiatWlLIDCw2A&pid=ImgRaw&r=0 height="100"></td>
+            <td><b>Chloe, Elizabeth Price</b><br><img src="/assets/Chloe-Price.jpg" height="100"></td>
             <td>Elle est sa meilleure amie avant son départ à Seattle, elle se connaissent depuis leurs enfances</td>
             <td>Max est très attacher à Chloe, on voit que Max s'est senti coupable de son départ, Max est attacher à Chloe, en foction de nos choix Max peut ressentir plus que de l'amitié pour Chloe</td>
         </tr>
         <tr>
-            <td><b>Ryan Caulfield</b><img src=https://th.bing.com/th/id/R.c3834e432c21154fc1f184fe1b04359a?rik=xh6Ek7zwxS%2bi8w&pid=ImgRaw&r=0 height="200"></td>
+            <td><b>Ryan Caulfield</b><img src="/assets/Rayan-Caufield.png" height="200"></td>
             <td>Il est le père de Max </td>
             <td>Max et son père sont très proche, il est très affectueux</td>
         </tr>
         <tr>
-            <td><b>Vanessa Caulfield</b><br><img src=https://static.wikia.nocookie.net/life-is-strange/images/4/49/Vanessa_caulfield.png/revision/latest/scale-to-width-down/55?cb=20150803034810 height="200"></td>
+            <td><b>Vanessa Caulfield</b><br><img src="/assets/Vanessa-Caulfield.webp" height="200"></td>
             <td>Elle est la mère de Max</td>
             <td>Elle tient beaucoup à Max qui lui manque. Elle soutient Max pour son départ à Blackwell mais est préoccupée.</td>
         </tr>
         <tr>
-            <td><b>Rachel, Dawn Amber</b><br><img src=https://th.bing.com/th/id/R.419fefa558aa4985595aa7ad797373fd?rik=pC5hxIVci97cPw&pid=ImgRaw&r=0 height="150"></td>
+            <td><b>Rachel, Dawn Amber</b><br><img src="/assets/Rachel-Amber.webp" height="150"></td>
             <td>Le lien entre Max et Rachel est Chloe</td>
             <td>Aucune relation</td>
         </tr>
             <tr>
-                <td><b>Warren Graham</b><br><img src=https://vignette4.wikia.nocookie.net/rememberme/images/0/04/Warren.png/revision/latest?cb=20150304054026 height="175"></td>
+                <td><b>Warren Graham</b><br><img src="/assets/Warren-Graham.webp" height="175"></td>
                 <td>Il est ami avec Max depuis le 4 septembre 2013 (depuis 1 mois)</td>
                 <td>Warren est amoureux de Max et en fonction des choix fait dans le jeu ça peut être reciproque . </td>
             </tr>
-            <td><b>Joyce Madsen</b><br><img src=https://th.bing.com/th/id/OIP.3ZKXgkdzuDpRU8puP3yeLwAAAA?pid=ImgDet&rs=1></td>
+            <td><b>Joyce Madsen</b><br><img src="/assets/Joyce-Madsen.jpg"></td>
             <td>Elle est la mère de Chloé sa meilleure amie</td>
             <td>Leurs relations sont cordiale, Max admire Joyce et voudrait bien être comme elle. </td>
         </tr>
         <tr>
-            <td><b>Victoria Chase</b><br><img src=https://th.bing.com/th/id/R.42610e08ebffba70757c5d05362d559a?rik=FpqDIT5HbxYQfQ&pid=ImgRaw&r=0 height="125"</td>
+            <td><b>Victoria Chase</b><br><img src="/assets/Victoria-Chase.webp" height="125"</td>
             <td>Elles sont camarades de classes </td>
             <td>Victoria prend de haut Max, elle en est jalouse</td>
         </tr>
         <tr>
-            <td><b>Nathan, Joshua Prescott</b><br><img src=https://th.bing.com/th/id/R.4d08c36ae56e40139e8ffd2c34afc3d3?rik=4N0YOJO6EZD66A&riu=http%3a%2f%2fvignette3.wikia.nocookie.net%2flife-is-strange%2fimages%2f5%2f5c%2fVlcsnap-2016-09-17-20h24m26s876.png%2frevision%2flatest%3fcb%3d20160918071516&ehk=vfOrFnEQBG0BBPBsTo4sMDu8gxyS4aw9svB9lC2mlfY%3d&risl=&pid=ImgRaw&r=0 height="125"</td>
+            <td><b>Nathan, Joshua Prescott</b><br><img src="/assets/Nathan-Prescott.png" height="125"</td>
             <td>Max et lui sont dans la  même classe</td>
             <td>Nathan est agressif avec Max </td>
         </tr>
         <tr>
-            <td><b>Kate Marsh</b><br><img src=https://th.bing.com/th/id/R.47ddcc7f5eaab93ab1258143f8ce36bb?rik=GEhiKEdIVZNbCQ&pid=ImgRaw&r=0 height=120></td>
+            <td><b>Kate Marsh</b><br><img src="/assets/Kate-Marsh.jpg" height=120></td>
             <td><br>Elles sont camarades et amies</td>
             <td><br>Max apprecie Kate, elles s'apprecient beaucoup </td>
         </tr>
         <tr>
-            <td><b>Mr Jefferson</b><br><img src=https://vignette.wikia.nocookie.net/life-is-strange-w/images/a/a3/Mark_jefferson.png/revision/latest/scale-to-width-down/310?cb=20180204044307&path-prefix=es height="125"></td>
+            <td><b>Mr Jefferson</b><br><img src="/assets/Mark_jefferson.webp" height="125"></td>
             <td>Il est le professeur de photographie de Max</td>
             <td>Max a de l'admiration pour le travail artistique de Mr Jefferson</td>
         </tr>
@@ -111,7 +111,7 @@
 
         </p>
         <div class="imgmax">
-            <img src=https://th.bing.com/th/id/R.30ace331011408dae4b23ba717ba189c?rik=nLngqWtRkPm6Yw&riu=http%3a%2f%2fstatic.playertheory.com%2fimages%2fLiS_MaxPhoto.jpg&ehk=J6CiePYC8tszL%2byCpzr3XL8Elfkp%2bpZeJG5DLbP5XeA%3d&risl=&pid=ImgRaw&r=0 height="300">
+            <img src="/assets/Max-Rembobinage.jpg" height="300">
         </div>
 
         <h3><strong>Consequences</strong></h3>
@@ -136,7 +136,7 @@
         <p>Max ecrit sur son journal, on peut voir ce qu'elle vit, ce qu'elle ressent par rapport aux evenements ou aux personnes qui l'entourent avec la chronologie. Nos actes ont une influence direct sur ce qui est ecrit dans son journal.
         </p>
         <div class ="journalmax">
-        <img src="https://static.wikia.nocookie.net/life-is-strange/images/7/71/Max_Journal.png/revision/latest/scale-to-width-down/599?cb=20170808155536&path-prefix=fr" height="315">
+        <img src="/assets/Max-Journal.webp" height="315">
         <iframe width="560" height="315" src="https://www.youtube.com/embed/4rM8doRu5Sc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
 
